25ee0530ecd1300b5870dee0423a1846b0d90fcf
hanura13 <hanuradiarjo@gmail.com>
2022-10-21 15:36:12 +0700

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Search } from '@mui/icons-material'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Home = () => {
+    return (
+        <Container>
+            Home <Search />
+        </Container>
+    )
+}
+
+export default Home
